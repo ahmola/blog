d36ab8346028c99d1a4ac538129f4345137d6ed2
Jason <66013853+ahmola@users.noreply.github.com>
2025-02-19 20:53:50 +0900

    mysql need to be in same docker network, connection is not worked with db

--- backend/apigateway/src/main/java/com/example/apigateway/util/JwtUtil.java
+++ backend/apigateway/src/main/java/com/example/apigateway/util/JwtUtil.java
@@ -2,10 +2,16 @@ package com.example.apigateway.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -25,15 +31,13 @@ public class JwtUtil {
         }
     }
 
-
-
     public Claims extractClaims(String token){
         try {
-            return Jwts.parserBuilder()
-                    .setSigningKey(secret.getBytes(StandardCharsets.UTF_8)) // 키 설정
+            return Jwts.parser()
+                    .verifyWith(getSignInKey())
                     .build()
-                    .parseClaimsJws(token)
-                    .getBody();
+                    .parseSignedClaims(token)
+                    .getPayload();
         }catch (Exception e){
             throw new RuntimeException("Token Cannot be extracted");
         }
@@ -42,5 +46,10 @@ public class JwtUtil {
     public String getUsernameFromToken(String token) {
         Claims claims = extractClaims(token);
         return claims.getSubject();
+    }
+
+    private SecretKey getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
--- backend/loginservice/src/main/java/com/example/loginservice/util/JwtUtil.java
+++ backend/loginservice/src/main/java/com/example/loginservice/util/JwtUtil.java
@@ -3,6 +3,7 @@ package com.example.loginservice.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
@@ -28,21 +30,21 @@ public class JwtUtil {
 
     public String generateToken(String username){
         return Jwts.builder()
-                .setSubject(username)   // 유저 이름
-                .setIssuedAt(new Date(System.currentTimeMillis()))    // 생성일
-                .setExpiration(new Date(System.currentTimeMillis()+expiration)) // 만료일
-                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)  // 서명, 위조 방지용
+                .subject(username)   // 유저 이름
+                .issuedAt(new Date(System.currentTimeMillis()))    // 생성일
+                .expiration(new Date(System.currentTimeMillis()+expiration)) // 만료일
+                .signWith(getSignInKey())  // 서명, 위조 방지용
                 .compact(); // 토큰 생성
     }
 
     // 토큰 내 정보(payload 또는 body)를 claim이라고 함
     public Claims extractClaims(String token){
         try{
-            return Jwts.parserBuilder()
-                    .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())) // 키 설정
+            return Jwts.parser()
+                    .verifyWith(getSignInKey())
                     .build()
-                    .parseClaimsJws(token) // 추출할 토큰
-                    .getBody(); // payload 추출
+                    .parseSignedClaims(token)
+                    .getPayload();// payload 추출
         }catch (Exception e){
             throw new RuntimeException("Something goes wrong during extracting claims");
         }
@@ -65,5 +67,11 @@ public class JwtUtil {
         }catch (Exception e){
             throw new RuntimeException("Invalid Token");
         }
+    }
+
+    // 비밀키 생성
+    private SecretKey getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
